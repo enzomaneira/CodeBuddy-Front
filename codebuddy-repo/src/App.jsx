34f932fe8f0e181732808de74b7aee43ab1a17f3
enzomaneira <enzo@maneira.com>
2023-10-25 20:48:34 -0300

    iniciando a home page

--- codebuddy-repo/src/App.jsx
+++ codebuddy-repo/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import pc_main from "/pc_main.png"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='content'>
+      <div className='text-container'>
+        <h1>Comece uma nova jornada de estudos</h1>
+        <p>Uma nova plataforma de aprendizado para você aprender programação</p>
+        <button>Cadastro</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='image-pc'>
+        <img src={pc_main} alt="CodeBoddu logo" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
