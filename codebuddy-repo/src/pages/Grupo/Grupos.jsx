374474e78bc10590edff7a5d123c1a560c458d49
enzomaneira <enzo@maneira.com>
2023-11-14 00:10:11 -0300

    implementando json-server

--- codebuddy-repo/src/pages/Grupo/Grupos.jsx
+++ codebuddy-repo/src/pages/Grupo/Grupos.jsx
@@ -1,10 +1,35 @@
-/* Grupos.jsx */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import styles from "./Grupos.module.css";
 import ContainerP from "../../components/container/ContainerP";
 
 const Grupos = () => {
+  const [users, setUsers] = useState([]);
+  const [grupos, setGrupos] = useState([]);
+  const [userGroups, setUserGroups] = useState([]);
+  const userId = 2; 
+
+  useEffect(() => {
+    fetch('http://localhost:3000/users') 
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data.users || []);
+        setGrupos(data.grupos || []);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  useEffect(() => {
+    const user = users.find(user => user.id === userId);
+    if (user) {
+      const groupNames = user.grupos.map(groupId => {
+        const group = grupos.find(group => group.id === groupId);
+        return group ? group.nome : null;
+      });
+      setUserGroups(groupNames);
+    }
+  }, [userId, users, grupos]);
+
   return (
     <div>
       <Navbar />
@@ -17,7 +42,11 @@ const Grupos = () => {
         />
       </div>
       <div className={styles.container}>
-        <ContainerP />
+        {userGroups.map((groupName, index) => (
+          <ContainerP key={index}>
+            <p>{groupName}</p>
+          </ContainerP>
+        ))}
       </div>
     </div>
   );
--- codebuddy-repo/src/pages/Perfil/Profile.jsx
+++ codebuddy-repo/src/pages/Perfil/Profile.jsx
@@ -1,30 +1,51 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import ContainerP from "../../components/container/ContainerM";
 import styles from "./Profile.module.css";
 import WhiteBall from "../../components/form/WhiteBall";
 import Navbar from '../../components/Navbar';
 import TextContainer from "../../components/container/TextContainer";
-import BotaoGrupo from "./botao/BotaoGrupo"
+import BotaoGrupo from "./botao/BotaoGrupo";
 import BotaoCriarGrupo from "../CriarGrupo/botao/BotaoCriarGrupo";
 
 const Profile = () => {
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+        const currentUser = data.find(user => user.id === "2");
+        setCurrentUser(currentUser);
+      });
+  }, []);
+
   return (
     <div className={styles.page}>
-      <Navbar/> 
+      <Navbar />
       <div className={styles.infosContainer}>
         <WhiteBall className={styles.whiteBall} />
         <ContainerP backgroundColor="orange" className={styles.container}>
           <div className={styles.textContainer}>
-            <div><TextContainer texto="Username"/></div>
-            <div><TextContainer texto="Grupos"/></div>
+            {currentUser && (
+              <>
+                <div>
+                  <TextContainer texto={`Name: ${currentUser.nome}`} />
+                </div>
+                <div>
+                  <TextContainer texto={`Grupos: ${currentUser.grupos}`} />
+                </div>
+              </>
+            )}
           </div>
         </ContainerP>
-        </div>
-        <div className={styles.botaoGrupo}>
-          <BotaoGrupo/>
+      </div>
+      <div className={styles.botaoGrupo}>
+        <BotaoGrupo />
         <div className={styles.botaoContainer}>
-          <BotaoCriarGrupo/>
-        <div className={styles.texto}>Criar Grupo </div>
+          <BotaoCriarGrupo />
+          <div className={styles.texto}>Criar Grupo </div>
         </div>
       </div>
     </div>
