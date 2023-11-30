f221494fc36e806e36a888311264deeaa1dec7ee
enzomaneira <enzo@maneira.com>
2023-11-30 20:56:18 -0300

    botao p voltar p desafio

--- codebuddy-repo/src/pages/CriarDesafio/componentsCriarDesafio/NavbarCriarDesafio.jsx
+++ codebuddy-repo/src/pages/CriarDesafio/componentsCriarDesafio/NavbarCriarDesafio.jsx
@@ -1,16 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import styles from "./NavbarCriarDesafio.module.css";
 import backarrow from "../../../img/backarrow.png";
 
 
 const NavbarCriarDesafio = ({ title, backButton }) => {
+    const { grupoNome } = useParams();
+  const [desafios, setDesafios] = useState([]);
+
+  useEffect(() => {
+    const fetchDesafios = async () => {
+      try {
+        const responseGrupos = await fetch(`http://localhost:3000/grupos?nome=${encodeURIComponent(grupoNome)}`);
+        const dataGrupos = await responseGrupos.json();
+        const grupo = dataGrupos[0];
+        if (grupo) {
+          const responseDesafios = await fetch(`http://localhost:3000/Desafios?grupoId=${encodeURIComponent(grupo.id)}`);
+          const dataDesafios = await responseDesafios.json();
+          setDesafios(dataDesafios || []);
+        } else {
+          console.warn(`Grupo não encontrado com o nome ${grupoNome}`);
+        }
+      } catch (error) {
+        console.error('Error fetching desafios:', error);
+      }
+    };
+
+    fetchDesafios();
+  }, [grupoNome]);
+
   return (
     <nav className={styles.navbar}>
         <span>{title}</span>
       {backButton && (
         <div className={styles.img}>
-        <Link to="/profile" className={styles.link}>
+        <Link to={`/Desafios/${encodeURIComponent(grupoNome)}`} className={styles.link}>
           <img src={backarrow} alt="Back Arrow" />
         </Link>
       </div>
