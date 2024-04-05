15307ab7f95d960d352000e7d3d5c63e70eaf662
Gabi <joaogzduram@gmail.com>
2024-04-04 23:19:11 -0300

    Adicionando formulário de login

--- codebuddy-repo/src/components/form/LoginForm.jsx
+++ codebuddy-repo/src/components/form/LoginForm.jsx
@@ -1,21 +1,47 @@
-import React from "react";
-import styles from "./LoginForm.module.css";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './LoginForm.module.css'; 
 
 const LoginForm = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <form className={styles.body}>
       <div className={styles.formGroup}>
-      <input type="text" id="login" name="login" placeholder="Login" />
+        <input type="text" id="login" name="login" placeholder="Login" />
       </div>
 
       <div className={styles.formGroup}>
-      <input type="password" id="senha" name="senha" placeholder="Senha" />
+        <input type="password" id="senha" name="senha" placeholder="Senha" />
+      </div>
+      <div className={styles.formGroup}>
+      <label>
+        <input className={`${styles.radioLabel} ${styles.checkboxLabel}`}
+          type="radio"
+          name="option"
+          value="Professor"
+          checked={selectedOption === 'Professor'}
+          onChange={handleOptionChange}
+        /> Professor
+      </label>
+        <label>
+          <input className={`${styles.radioLabel} ${styles.checkboxLabel}`}
+            type="radio"
+            name="option"
+            value="Aluno"
+            checked={selectedOption === 'Aluno'}
+            onChange={handleOptionChange}
+          />Aluno
+        </label>
       </div>
       <Link to="/Profile">
-      <button className={styles.button} type="submit">
-        Entrar
-      </button>
+        <button className={styles.button} type="submit">
+          Entrar
+        </button>
       </Link>
     </form>
   );
